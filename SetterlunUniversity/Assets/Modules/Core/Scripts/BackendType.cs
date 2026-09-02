8276b2cb3e179ecea710bca0c76002ce199d33bd
DESKTOP-KS4HGEV\asad <asad.regalsystems@gmail.com>
2026-09-02 12:19:50 +0500

    Deploy Unity WebGL build at root

--- SetterlunUniversity/Assets/Modules/Core/Scripts/BackendType.cs
+++
@@ -1,7 +0,0 @@
-public enum BackendType
-{
-    CustomAPI,
-    Firebase,
-    LocalDemo,
-    PortalBridge
-}
--- setterlun-backend/routes/timeCapsules.js
+++
@@ -1,9 +0,0 @@
-﻿const express = require('express');
-const router = express.Router();
-const { createCapsule, getCapsulesByUser, listCapsules } = require('../controllers/timeCapsulesController');
-
-router.post('/', createCapsule);
-router.get('/:user_id', getCapsulesByUser);
-router.get('/', listCapsules);
-
-module.exports = router;
